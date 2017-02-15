1da65a3cbb5e1685c3049d61f90493ce6fc7005b
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-15 15:37:00 -0500

    step one complete need to revisit this to make it into a hooror app

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+$(function() {
 
+    $('.switch').on('click', function() {
 
+        $('body').toggleClass('dark');
+        $('body').toggleClass('light');
+
+        if ($('.switch button').attr('class') === 'on') {
+            $('.switch button').removeClass('on').addClass('off');
+            $('.status').text("You're not safe in the dark");
+
+        } else {
+            $('.switch button').removeClass('off').addClass('on');
+            $('.status').text("Lets keep the lights bright!");
+        }
+    })
+});
